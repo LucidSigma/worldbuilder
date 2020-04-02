e2634cf2b5b7ff6f272aecb5725c49006b5f7055
Matt Schafer <lucidsigma17@gmail.com>
2020-04-02 20:14:14 +1000

    Modify home page.

--- app/controllers/homeController.js
+++ app/controllers/homeController.js
@@ -3,7 +3,6 @@
 const express = require("express");
 
 const router = express.Router();
-//router.use(express.static(__dirname + "./public/"));
 
 router.get("/", (request, response) => {
 	response.render("home");
