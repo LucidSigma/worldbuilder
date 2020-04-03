3ac6d2225e63dc9a82a2cb32d91b9e902315baca
Matt Schafer <lucidsigma17@gmail.com>
2020-04-03 14:48:40 +1000

    Add planet creation pages.

--- app/controllers/planetController.js
+++ app/controllers/planetController.js
@@ -6,6 +6,7 @@ const Planet = require("../models/planet");
 
 const router = express.Router();
 
+// INDEX
 router.get("/", async (request, response) => {
 	response.locals.title += " - Planet Index";
 	const planets = await Planet.find({ });
@@ -14,5 +15,43 @@ router.get("/", async (request, response) => {
 		planets: planets
 	});
 });
+
+// NEW
+// TODO: Add logged in middleware.
+router.get("/new", (request, response) => {
+	response.render("planets/new");
+});
+
+// CREATE
+// TODO: Add logged in middleware.
+router.post("/", async (request, response) => {
+	let planetData = request.body.planet;
+	planetData.comments = [];
+	// Add in planet author here.
+	/*
+	planetData.author = {
+		id: request.user._id,
+		username: request.user.username
+	};
+	*/
+
+	try {
+		const createdPlanet = await Planet.create(planetData);
+		request.flash("success", "Planet created successfully.");
+	}
+	catch (error) {
+		request.flash("error", "Failed to create planet, please try again.");
+	}
+
+	response.redirect("/planets");
+});
+
+// SHOW
+
+// EDIT
+
+// UPDATE
+
+// DESTROY
 
 module.exports = router;
